19f9d7462c52352b5307b6b5a9c4ec67c3af26d1
Oliver Ruebel <oruebel@lbl.gov>
2024-04-26 22:48:57 -0700

    Add LINDI time_remote_slicing benchmark

--- src/nwb_benchmarks/benchmarks/time_remote_slicing.py
+++ src/nwb_benchmarks/benchmarks/time_remote_slicing.py
@@ -6,13 +6,12 @@ from typing import Tuple
 from nwb_benchmarks.core import (
     get_object_by_name,
     get_s3_url,
-    read_hdf5_fsspec_with_cache,
     read_hdf5_nwbfile_fsspec_no_cache,
     read_hdf5_nwbfile_fsspec_with_cache,
+    read_hdf5_nwbfile_lindi,
     read_hdf5_nwbfile_remfile,
     read_hdf5_nwbfile_remfile_with_cache,
     read_hdf5_nwbfile_ros3,
-    read_hdf5_remfile_with_cache,
 )
 
 # TODO: add the others
@@ -28,6 +27,15 @@ params = (
     ["ElectricalSeriesAp"],
     [(slice(0, 30_000), slice(0, 384))],  # ~23 MB
 )
+
+
+# Parameters for LINDI pointing to a remote LINDI reference file system JSON file
+lindi_remote_rfs_param_names = param_names
+lindi_remote_rfs_params = [
+    ["https://kerchunk.neurosift.org/dandi/dandisets/000939/assets/11f512ba-5bcf-4230-a8cb-dc8d36db38cb/zarr.json"],
+    ["accelerometer"],
+    [(slice(0, 30_000), slice(0, 3))],
+]
 
 
 class FsspecNoCacheContinuousSliceBenchmark:
@@ -112,6 +120,27 @@ class Ros3ContinuousSliceBenchmark:
 
     def setup(self, s3_url: str, object_name: str, slice_range: Tuple[slice]):
         self.nwbfile, self.io, _ = read_hdf5_nwbfile_ros3(s3_url=s3_url)
+        self.neurodata_object = get_object_by_name(nwbfile=self.nwbfile, object_name=object_name)
+        self.data_to_slice = self.neurodata_object.data
+
+    def time_slice(self, s3_url: str, object_name: str, slice_range: Tuple[slice]):
+        """Note: store as self._temp to avoid tracking garbage collection as well."""
+        self.data_to_slice[slice_range]
+
+
+class NWBLindiFileReadRemoteReferenceFileSystemBenchmark:
+    """
+    Time the read of a data slice from a remote NWB file with pynwb using lindi with a remote JSON reference
+    filesystem available.
+    """
+
+    rounds = 1
+    repeat = 3
+    param_names = lindi_remote_rfs_param_names
+    params = lindi_remote_rfs_params
+
+    def setup(self, s3_url: str, object_name: str, slice_range: Tuple[slice]):
+        self.nwbfile, self.io, self.client = read_hdf5_nwbfile_lindi(rfs=s3_url)
         self.neurodata_object = get_object_by_name(nwbfile=self.nwbfile, object_name=object_name)
         self.data_to_slice = self.neurodata_object.data
 
