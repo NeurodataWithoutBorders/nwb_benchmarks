b187b29e22746345711a3d9489d45ff1e7b3a757
Cody Baker <cody.c.baker.phd@gmail.com>
2024-05-18 12:16:35 -0400

    dandi improvement

--- src/nwb_benchmarks/core/_dandi.py
+++ src/nwb_benchmarks/core/_dandi.py
@@ -16,11 +16,11 @@ def get_s3_url(dandiset_id: str, dandi_path: str) -> str:
     """
     assert len(dandiset_id) == 6, f"The specified 'dandiset_id' ({dandiset_id}) should be the six-digit identifier."
 
-    api_url = (
-        "https://api-staging.dandiarchive.org/api"
-        if int(dandiset_id) >= 200_000
-        else "https://api.dandiarchive.org/api"
-    )
+    if int(dandiset_id) >= 200_000:
+        api_url = "https://api-staging.dandiarchive.org/api"
+    else:
+        api_url = "https://api.dandiarchive.org/api"
+
     client = DandiAPIClient(api_url=api_url)
     dandiset = client.get_dandiset(dandiset_id=dandiset_id)
     asset = dandiset.get_asset_by_path(path=dandi_path)
