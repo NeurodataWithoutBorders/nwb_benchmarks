157fbf54480bd3d0a29088fcb1d0296e8988660a
mavaylon1 <mavaylon1@berkeley.edu>
2024-11-20 19:15:08 -0800

    file

--- src/nwb_benchmarks/benchmarks/time_remote_slicing.py
+++ src/nwb_benchmarks/benchmarks/time_remote_slicing.py
@@ -62,7 +62,7 @@ lindi_remote_rfs_parameter_cases = dict(
     EcephysTestCase=dict(
         s3_url=get_s3_url(
             dandiset_id="213889",
-            dandi_path="sub-IBL-ecephys/sub-IBL-ecephys_ses-3e7ae7c0_desc-18000000-frames-13653-by-384-chunking.lindi.json",
+            dandi_path="sub-ecephys/c493119b-4b99-4b14-bc03-65bb28cfbd29.lindi.json",
         ),
         object_name="ElectricalSeries",
         slice_range=(slice(0, 30_000), slice(0, 384)),
