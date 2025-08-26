eb36143e9cc4a93b522d38e3712c2a1d63bf3dc4
rly <rly@lbl.gov>
2025-08-26 14:34:19 -0700

    Specify network interface

--- src/nwb_benchmarks/core/_network_profiler.py
+++ src/nwb_benchmarks/core/_network_profiler.py
@@ -50,6 +50,9 @@ class NetworkProfiler:
         """Start the capture with tshark in a subprocess."""
         tshark_path = tshark_path or "tshark"
         tsharkCall = [str(tshark_path), "-w", str(self.capture_file_path)]
+        networkInterface = os.environ.get("NWB_BENCHMARKS_NETWORK_INTERFACE")
+        if networkInterface:
+            tsharkCall.extend(["-i", networkInterface])
         self.__tshark_process = subprocess.Popen(tsharkCall, stderr=subprocess.DEVNULL)
         time.sleep(0.2)  # not sure if this is needed but just to be safe
 
