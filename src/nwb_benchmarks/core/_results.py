dda14e6aab720e90fa6447c1f17f97104736b6a0
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2025-08-29 13:08:37 -0400

    Skip already uploaded filenames (#124)
    To save bandwidth and also allow more efficient 'retry' for failed
    results

--- src/nwb_benchmarks/core/_results.py
+++ src/nwb_benchmarks/core/_results.py
@@ -6,7 +6,7 @@ import warnings
 
 import requests
 
-from ..globals import ENVIRONMENTS_DIR, MACHINES_DIR, RESULTS_CACHE_DIR
+from ..globals import CACHE_DIR, ENVIRONMENTS_DIR, MACHINES_DIR, RESULTS_CACHE_DIR
 
 
 def clean_results():
@@ -19,29 +19,48 @@ def clean_results():
 
 
 def upload_results():
-    for results_file_path in itertools.chain(
-        RESULTS_CACHE_DIR.rglob(pattern="*.json"),
-        MACHINES_DIR.rglob(pattern="*.json"),
-        ENVIRONMENTS_DIR.rglob(pattern="*.json"),
-    ):
-        with results_file_path.open("r") as file_stream:
-            json_content = json.load(file_stream)
+    upload_tracker_file_path = CACHE_DIR / "upload_tracker.json"
+    if upload_tracker_file_path.exists() is False:
+        upload_tracker_file_path.write_text(data="{}")
 
-        filename = results_file_path.name
-        response = requests.post(
-            url=f"https://codycbakerphd.pythonanywhere.com/data/contribute?filename={filename}",
-            json={"json_content": json_content},
-            timeout=30,
-        )
-        if response.status_code == 200:
-            print(f"Results posted successfully! {filename}")
-        else:
-            message = (
-                f"Failed to post results. {filename} "
-                "Please raise an issue on https://github.com/NeurodataWithoutBorders/nwb_benchmarks/issues. "
-                f"Status code: {response.status_code} "
-                f"Response: {response.text}"
+    with upload_tracker_file_path.open(mode="r") as file_stream:
+        upload_tracker = json.load(fp=file_stream)
+
+    try:
+        all_file_paths_to_upload = list(
+            itertools.chain(
+                RESULTS_CACHE_DIR.rglob(pattern="*.json"),
+                MACHINES_DIR.rglob(pattern="*.json"),
+                ENVIRONMENTS_DIR.rglob(pattern="*.json"),
             )
-            warnings.warn(message=message, stacklevel=2)
+        )
+        file_paths_to_upload = [
+            file_path for file_path in all_file_paths_to_upload if upload_tracker.get(file_path.name, False) is False
+        ]
 
-        time.sleep(1)
+        for file_path in file_paths_to_upload:
+            with file_path.open("r") as file_stream:
+                json_content = json.load(file_stream)
+
+            filename = file_path.name
+            response = requests.post(
+                url=f"https://codycbakerphd.pythonanywhere.com/data/contribute?filename={filename}",
+                json={"json_content": json_content},
+                timeout=30,
+            )
+            if response.status_code == 200:
+                upload_tracker[filename] = True
+                print(f"Results posted successfully! {filename}")
+            else:
+                message = (
+                    f"Failed to post results. {filename} "
+                    "Please raise an issue on https://github.com/NeurodataWithoutBorders/nwb_benchmarks/issues. "
+                    f"Status code: {response.status_code} "
+                    f"Response: {response.text}"
+                )
+                warnings.warn(message=message, stacklevel=2)
+
+            time.sleep(1)
+    finally:
+        with upload_tracker_file_path.open(mode="w") as file_stream:
+            json.dump(obj=upload_tracker, fp=file_stream, indent=1)
