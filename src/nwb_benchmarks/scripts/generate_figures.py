b9742f0b71cfd04102085607524c380f22ff50ad
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-09-25 10:44:10 -0700

    add slice vs time plots

--- src/nwb_benchmarks/scripts/generate_figures.py
+++ src/nwb_benchmarks/scripts/generate_figures.py
@@ -50,11 +50,11 @@ def add_mean_sem_annotations(value, group, order, **kwargs):
         plt.text(x=stats.loc[label, 'max'], y=i, s=mean_sem_text,
                 verticalalignment='center', horizontalalignment='left', fontsize=8)
 
-def create_benchmark_dist_plot(df, group, metric, metric_order, filename, row=None, sharex=True, add_annotations=True):
+def plot_benchmark_dist(df, group, metric, metric_order, filename, row=None, sharex=True, add_annotations=True):
     g = sns.catplot(
         data=df.collect().to_pandas(), 
         x="value", y=group, col='modality', row=row, hue=group,
-        sharex=sharex, palette="Set2", kind='box', showfliers=False,
+        sharex=sharex, palette="Paired", kind='box', showfliers=False,
         boxprops=dict(linewidth=0), order=metric_order, legend=False
     )
     
@@ -65,6 +65,19 @@ def create_benchmark_dist_plot(df, group, metric, metric_order, filename, row=No
     for ax in g.axes.flat:
         wrapped_title = '\n'.join(textwrap.wrap(ax.get_title(), width=50))
         ax.set_title(wrapped_title)
+    
+    sns.despine()
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
+
+def plot_benchmark_slices_vs_time(df, group, metric, metric_order, filename, row=None, sharex=True):
+    g = sns.catplot(
+        data=df.collect().to_pandas(), 
+        x="slice_number", y='value', col='modality', row=row, hue=group, hue_order=metric_order,
+        sharex=sharex, palette="Paired", kind='point', sharey=False)
+    
+    g.set(xlabel="Relative slice size", ylabel='Time (s)')
     
     sns.despine()
     plt.tight_layout()
@@ -88,10 +101,16 @@ def filter_slice_tests(benchmark_type, benchmark_method):
             .with_columns(
                 pl.col('benchmark_name_test')
                 .map_elements(lambda x: clean_benchmark_name_test(x, benchmark_method))
-                .alias('benchmark_name_test')
-            ))
+                .alias('benchmark_name_test'))
+            .with_columns(
+                pl.col("parameter_case_slice_range").list.first().str.extract(r"slice\(0, (\d+),", group_index=1).cast(pl.Int64)
+                .alias("scaling_value"))
+            .with_columns(
+                (pl.col("scaling_value").rank(method="dense") - 1).over('modality')
+                .alias("slice_number"))
+            )
 
-def make_read_benchmark_plot(test, method, order, benchmark_type='time_remote_file_reading', 
+def plot_read_benchmarks(test, method, order, benchmark_type='time_remote_file_reading', 
                       prefix='time_read', network_tracking=False):
     filtered_df = filter_read_tests(benchmark_type, test, method, prefix)
     
@@ -107,9 +126,9 @@ def make_read_benchmark_plot(test, method, order, benchmark_type='time_remote_fi
     if network_tracking:
         plot_kwargs.update({'row': 'variable', 'sharex': False, 'add_annotations': False})
     
-    create_benchmark_dist_plot(**plot_kwargs)
+    plot_benchmark_dist(**plot_kwargs)
 
-def make_slice_benchmark_plots(method, order, benchmark_type='time_remote_slicing', network_tracking=False):
+def plot_slice_benchmarks(method, order, benchmark_type='time_remote_slicing', network_tracking=False):
     filtered_df = filter_slice_tests(benchmark_type, method)
     
     filename_prefix = "network_tracking_" if network_tracking else ""
@@ -124,12 +143,37 @@ def make_slice_benchmark_plots(method, order, benchmark_type='time_remote_slicin
     if network_tracking:
         plot_kwargs.update({'row': 'variable', 'sharex': 'row', 'add_annotations': False})
     
-    create_benchmark_dist_plot(**plot_kwargs)
+    plot_benchmark_dist(**plot_kwargs)
+
+def plot_download_vs_stream_benchmarks(method, order, benchmark_type='time_remote_slicing', network_tracking=False):
+    slice_df = filter_slice_tests(benchmark_type, method)
+    
+    # plot just the slice information
+    filename_prefix = "network_tracking_" if network_tracking else ""
+    plot_kwargs = {
+        'df': slice_df,
+        'group': 'benchmark_name_test',
+        'metric': f'{method} Continuous Slice Benchmark',
+        'metric_order': order,
+        'filename': output_directory / f"{filename_prefix}slicing_vs_time_{method}.png"
+    }
+    
+    if network_tracking:
+        plot_kwargs.update({'row': 'variable', 'sharex': 'row', 'add_annotations': False})
+    
+    plot_benchmark_slices_vs_time(**plot_kwargs)
+
+    # plot slice + read vs. download information
+    # TODO - need to capture download time for each test case
+
+
+
 
 # Common benchmark orders
-hdf5_order = ['remfile no cache', 'remfile with cache', 'ros3',
+hdf5_order = ['remfile no cache', 'remfile with cache', 
               'fsspec https no cache', 'fsspec https with cache',
-              'fsspec s3 no cache', 'fsspec s3 with cache']
+              'fsspec s3 no cache', 'fsspec s3 with cache',
+              'ros3',]
 
 slicing_hdf5_order = ['Fsspec S3 No Cache', 'Fsspec S3 With Cache',
                       'Fsspec S3 Preloaded No Cache', 'Fsspec S3 Preloaded With Cache',
@@ -142,54 +186,59 @@ zarr_order = ['https', 'https force no consolidated', 's3', 's3 force no consoli
 zarr_pynwb_order = ['s3', 's3 force no consolidated']
 lindi_order = ['h5py', 'pynwb']
 
+############################### WHEN TO DOWNLOAD VS. STREAM DATA? #############################
+
+# selecting a specific approach, for each modality when should I download vs. stream
+# 1 plot for each modality (ecephys, ophys, icephys) 
+# baseline download line (number of slices that pushes you over the download time?)
+
+# time to open + slice vs. number of slices
+# TODO - need the download time for the specific approach we want to focus on
+plot_download_vs_stream_benchmarks('HDF5PyNWB', slicing_hdf5_order)
+plot_download_vs_stream_benchmarks('Lindi', ['Local JSON'])
+plot_download_vs_stream_benchmarks('ZarrPyNWB', ['S3', 'S3 Force No Consolidated'])
+
+
 ######################### WHICH LIBRARY SHOULD I USE TO STREAM DATA? #########################
 
 # 1. Remote file reading - which methods are fastest?
-make_read_benchmark_plot('HDF5H5pyFileReadBenchmark', 'hdf5_h5py', hdf5_order)
-make_read_benchmark_plot('HDF5PyNWBFileReadBenchmark', 'hdf5_pynwb', hdf5_order)
-make_read_benchmark_plot('LindiLocalJSONFileReadBenchmark', 'lindi', lindi_order)
-make_read_benchmark_plot('ZarrZarrPythonFileReadBenchmark', 'zarr', zarr_order)
-make_read_benchmark_plot('ZarrPyNWBFileReadBenchmark', 'zarr_pynwb', zarr_pynwb_order)
+plot_read_benchmarks('HDF5H5pyFileReadBenchmark', 'hdf5_h5py', hdf5_order)
+plot_read_benchmarks('HDF5PyNWBFileReadBenchmark', 'hdf5_pynwb', hdf5_order)
+plot_read_benchmarks('LindiLocalJSONFileReadBenchmark', 'lindi', lindi_order)
+plot_read_benchmarks('ZarrZarrPythonFileReadBenchmark', 'zarr', zarr_order)
+plot_read_benchmarks('ZarrPyNWBFileReadBenchmark', 'zarr_pynwb', zarr_pynwb_order)
 
 # 2. Remote file slicing
-make_slice_benchmark_plots('HDF5PyNWB', slicing_hdf5_order)
-make_slice_benchmark_plots('Lindi', ['Local JSON'])
-make_slice_benchmark_plots('ZarrPyNWB', ['S3', 'S3 Force No Consolidated'])
+plot_slice_benchmarks('HDF5PyNWB', slicing_hdf5_order)
+plot_slice_benchmarks('Lindi', ['Local JSON'])
+plot_slice_benchmarks('ZarrPyNWB', ['S3', 'S3 Force No Consolidated'])
 
 # 3. Network tracking - which methods make the most requests?
 benchmark_type = "network_tracking_remote_file_reading"
 prefix = "track_network_read"
-make_read_benchmark_plot('HDF5H5pyFileReadBenchmark', 'hdf5_h5py', hdf5_order,
+plot_read_benchmarks('HDF5H5pyFileReadBenchmark', 'hdf5_h5py', hdf5_order,
                   benchmark_type=benchmark_type, prefix=prefix, network_tracking=True)
 
-make_read_benchmark_plot('HDF5PyNWBFileReadBenchmark', 'hdf5_pynwb', hdf5_order,
+plot_read_benchmarks('HDF5PyNWBFileReadBenchmark', 'hdf5_pynwb', hdf5_order,
                   benchmark_type=benchmark_type, prefix=prefix, network_tracking=True)
 
-make_read_benchmark_plot('LindiLocalJSONFileReadBenchmark', 'lindi', lindi_order,
+plot_read_benchmarks('LindiLocalJSONFileReadBenchmark', 'lindi', lindi_order,
                   benchmark_type=benchmark_type, prefix=prefix, network_tracking=True)
 
-make_read_benchmark_plot('ZarrZarrPythonFileReadBenchmark', 'zarr', zarr_order,
+plot_read_benchmarks('ZarrZarrPythonFileReadBenchmark', 'zarr', zarr_order,
                   benchmark_type=benchmark_type, prefix=prefix, network_tracking=True)
 
-make_read_benchmark_plot('ZarrPyNWBFileReadBenchmark', 'zarr_pynwb', zarr_pynwb_order,
+plot_read_benchmarks('ZarrPyNWBFileReadBenchmark', 'zarr_pynwb', zarr_pynwb_order,
                   benchmark_type=benchmark_type, prefix=prefix, network_tracking=True)
 
 benchmark_type = "network_tracking_remote_slicing"
-make_slice_benchmark_plots('HDF5PyNWB', slicing_hdf5_order,
+plot_slice_benchmarks('HDF5PyNWB', slicing_hdf5_order,
                            benchmark_type=benchmark_type, network_tracking=True)
-make_slice_benchmark_plots('Lindi', ['Local JSON'],
+plot_slice_benchmarks('Lindi', ['Local JSON'],
                            benchmark_type=benchmark_type, network_tracking=True)
-make_slice_benchmark_plots('ZarrPyNWB', ['S3', 'S3 Force No Consolidated'],
+plot_slice_benchmarks('ZarrPyNWB', ['S3', 'S3 Force No Consolidated'],
                            benchmark_type=benchmark_type, network_tracking=True)
 
-
-########################### WHEN SHOULD I DOWNLOAD VS. STREAM DATA? ##########################
-
-# TODO - need the download time for the specific approach we want to focus on
-# selecting a specific approach, for each modality when should I download vs. stream
-# 1 plot for each modality (ecephys, ophys, icephys) 
-# time to open + slice vs. number of slices
-# baseline download line (number of slices that pushes you over the download time?)
 
 ###################### HOW DOES PERFORMANCE CHANGE ACROSS VERSIONS/TIME ######################
 
