7fc691169c21148cf35d5c05ceeb8c9f8a8e7eb5
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-10-24 08:16:32 -0700

    Add download benchmarks using DANDI api (#169)
    Adds benchmarks to get download times for HDF5 and Zarr NWB files to
    compare against slicing extrapolations. To be used when deciding whether
    to download vs. stream an NWB file depending on the amount of data being
    accessed.
    
    We had discussed making these either in a separate benchmarks folder or
    adding an environment variable. I thought setting the environment
    variable with the skip decorator was cleaner since there are other
    download related benchmarks and only two full file download tests. The
    download benchmarks can then be run manually with:
    
    ```bash
    RUN_DOWNLOAD_BENCHMARKS=true nwb_benchmarks run --bench "time_download.HDF5DownloadDandiAPIBenchmark.time_download_hdf5_dandi_api"
    ````
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Ryan Ly <rly@lbl.gov>

---
+++ src/nwb_benchmarks/benchmarks/time_download.py
@@ -0,0 +1,67 @@
+"""Basic benchmarks for timing download of remote NWB files using different methods."""
+
+import shutil
+
+from asv_runner.benchmarks.mark import skip_benchmark_if
+from dandi.download import download
+
+from nwb_benchmarks import RUN_DOWNLOAD_BENCHMARKS
+from nwb_benchmarks.core import BaseBenchmark
+from nwb_benchmarks.setup import get_temporary_directory
+
+from .params_remote_download import hdf5_params, lindi_remote_rfs_params, zarr_params
+
+
+class BaseDownloadDandiAPIBenchmark(BaseBenchmark):
+    """
+    Base class for timing the download of remote NWB files using the DANDI API.
+    """
+
+    def setup(self, params: dict[str, str]):
+        self.tmpdir = get_temporary_directory()
+        self.teardown(params)
+
+    def teardown(self, params: dict[str, str]):
+        if hasattr(self, "tmpdir"):
+            shutil.rmtree(path=self.tmpdir.name, ignore_errors=True)
+            self.tmpdir.cleanup()
+
+
+class HDF5DownloadDandiAPIBenchmark(BaseDownloadDandiAPIBenchmark):
+    """
+    Time the download of remote HDF5 NWB files using the DANDI API.
+    """
+
+    params = hdf5_params
+
+    # NOTE - these benchmarks download the full file which can take a long time. Only run explicitly using RUN_DOWNLOAD_BENCHMARKS=true when needed.
+    @skip_benchmark_if(not RUN_DOWNLOAD_BENCHMARKS)
+    def time_download_hdf5_dandi_api(self, params: dict[str, str]):
+        """Download a remote HDF5 NWB file using the DANDI API."""
+        download(urls=params["https_url"], output_dir=self.tmpdir.name)
+
+
+class ZarrDownloadDandiAPIBenchmark(BaseDownloadDandiAPIBenchmark):
+    """
+    Time the download of remote Zarr NWB files using the DANDI API.
+    """
+
+    params = zarr_params
+
+    # NOTE - these benchmarks download the full file which can take a long time. Only run explicitly using RUN_DOWNLOAD_BENCHMARKS=true when needed.
+    @skip_benchmark_if(not RUN_DOWNLOAD_BENCHMARKS)
+    def time_download_zarr_dandi_api(self, params: dict[str, str]):
+        """Download a remote Zarr NWB directory using the DANDI API."""
+        download(urls=params["https_url"], output_dir=self.tmpdir.name)
+
+
+class LindiDownloadDandiAPIBenchmark(BaseDownloadDandiAPIBenchmark):
+    """
+    Time the download of a remote LINDI JSON file.
+    """
+
+    params = lindi_remote_rfs_params
+
+    def time_download_lindi_dandi_api(self, params: dict[str, str]):
+        """Download a remote Lindi file using the DANDI API."""
+        download(urls=params["https_url"], output_dir=self.tmpdir.name)
